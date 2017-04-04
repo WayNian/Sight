a924aaef8b8d0538195add91c9dfe8167ec5f52d
waynian <1245343496@qq.com>
2017-04-04 23:55:04 +0800

    fragment懒加载

--- app/src/main/java/com/sight/waynian/sight/base/BaseFragment.java
+++ app/src/main/java/com/sight/waynian/sight/base/BaseFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,20 +14,71 @@ import android.view.ViewGroup;
  */
 
 public abstract class BaseFragment extends Fragment {
+    private static final String TAG = BaseFragment.class.getCanonicalName();
     protected Context mContext;
+    /**
+     * rootView是否初始化标志，防止回调函数在rootView为空的时候触发
+     */
+    private boolean hasCreateView;
+
+    /**
+     * 当前Fragment是否处于可见状态标志，防止因ViewPager的缓存机制而导致回调函数的触发
+     */
+    private boolean isFragmentVisible;
+
+    /**
+     * onCreateView()里返回的view，修饰为protected,所以子类继承该类时，在onCreateView里必须对该变量进行初始化
+     */
+    protected View rootView;
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        Log.d(TAG, "setUserVisibleHint() -> isVisibleToUser: " + isVisibleToUser);
+        if (rootView == null) {
+            return;
+        }
+        hasCreateView = true;
+        if (isVisibleToUser) {
+            onFragmentVisibleChange(true);
+            isFragmentVisible = true;
+            return;
+        }
+        if (isFragmentVisible) {
+            onFragmentVisibleChange(false);
+            isFragmentVisible = false;
+        }
+    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         mContext = getActivity();
+        hasCreateView = false;
+        isFragmentVisible = false;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         return initView();
+    }
+
+    /**
+     * onViewCreated在onCreateView执行完后立即执行。
+     * onCreateView返回的就是fragment要显示的view。
+     * @param view
+     * @param savedInstanceState
+     */
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (!hasCreateView && getUserVisibleHint()) {
+            onFragmentVisibleChange(true);
+            isFragmentVisible = true;
+        }
     }
 
     /**
@@ -34,5 +86,21 @@ public abstract class BaseFragment extends Fragment {
      * @return
      */
     protected abstract View initView();
+
+    /**************************************************************
+     *  自定义的回调方法，子类可根据需求重写
+     *************************************************************/
+
+    /**
+     * 当前fragment可见状态发生变化时会回调该方法
+     * 如果当前fragment是第一次加载，等待onCreateView后才会回调该方法，其它情况回调时机跟 {@link #setUserVisibleHint(boolean)}一致
+     * 在该回调方法中你可以做一些加载数据操作，甚至是控件的操作，因为配合fragment的view复用机制，你不用担心在对控件操作中会报 null 异常
+     *
+     * @param isVisible true  不可见 -> 可见
+     *                  false 可见  -> 不可见
+     */
+    protected void onFragmentVisibleChange(boolean isVisible) {
+        Log.w(TAG, "onFragmentVisibleChange -> isVisible: " + isVisible);
+    }
 
 }
